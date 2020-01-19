ce56f76199e8e9ebc4f03bd2f46b754d43cbe43c
bili.zhao@pdabc.com <bili.zhao@pdabc.com>
2020-01-19 17:14:06 +0800

    support gluon

---
+++ mxnet/gluon/fpn.py
@@ -0,0 +1,13 @@
+import mxnet as mx
+from mxnet.gluon import nn, loss, opt
+from mxboard import SummaryWriter
+from mxnet import autograd, nd
+import numpy as np
+
+class FPN(nn.Block):
+    def __init__(self, **kward):
+        super(FPN, kward)
+        pass
+    
+    def forward(self,x):
+        pass
